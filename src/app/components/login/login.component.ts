f24fd75794aa8d1cb815ec6b6aace8a6e136fbee
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-11-11 14:28:50 +0530

    login functionality added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit() {
+
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.authenticateUser(user).subscribe(data => {
+
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user);
+
+        this.flashMessage.show('You are now Logged In!', {cssClass: 'alert-success', timeout: 5000});
+        this.router.navigate(['dashboard']);
+
+      } else {
+        this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 5000});
+        this.router.navigate(['login']);
+      }
+
+    });
+  }
 }
